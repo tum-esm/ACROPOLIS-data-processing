e64ee3c432b4df578262f52737138b3a96ebf28f
Patrick <patrick.aigner@tum.de>
2024-03-11 16:22:35 +0100

    major update

--- notebooks/utils/calibration_processing.py
+++ notebooks/utils/calibration_processing.py
@@ -1,37 +1,43 @@
 import warnings
+import os
+import polars as pl
+import numpy
+
+DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY")
 
 warnings.simplefilter("ignore", category=FutureWarning)
 
+# load calibration bottle concentrations (preprocessed)
+df_gas = pl.read_csv(os.path.join(DATA_DIRECTORY, "input", "averaged_gases.csv"))
 
-def two_point_calibration(measured_values, true_values):
+
+# define functions
+
+
+def process_bottle(data: list):
+    # 2nd bottle
+    if 50 < len(data) < 70:
+        x = data[int(len(data) * 0.3) : int(len(data) * 0.95)]
+        return numpy.median(x)
+    # 1st bottle
+    elif 70 < len(data) < 130:
+        x = data[int(len(data) * 0.5) : int(len(data) * 0.95)]
+        return numpy.median(x)
+    else:
+        return 0.0
+
+
+def two_point_calibration(measured_values: list, true_values: list):
     # Check if input lists have length 2
     if len(measured_values) != 2 or len(true_values) != 2:
         raise ValueError("Both measured_values and true_values must have length 2")
 
     # Calculate calibration parameters (slope and intercept)
-    #
+
     slope = (true_values[1] - true_values[0]) / (
         measured_values[1] - measured_values[0]
     )
+    # y_true = m * y_meas + t
     intercept = true_values[0] - slope * measured_values[0]
 
     return slope, intercept
-
-
-def average_bottle(conc_list):
-    if len(conc_list) == 0:
-        print("Length of list to average is 0.")
-        return 0
-
-    uncut_avg = sum(conc_list) / len(conc_list)
-    print(f"Uncut average: {uncut_avg}")
-
-    if len(conc_list) > 0:
-        conc_list_cut = conc_list[
-            int(len(conc_list) * 0.3) : int(len(conc_list) * 0.95)
-        ]
-        cut_avg = sum(conc_list_cut) / len(conc_list_cut)
-        print(f"Cut average: {cut_avg}")
-
-        return cut_avg
-    return 0
