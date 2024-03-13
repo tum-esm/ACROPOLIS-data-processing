f499cba6432fe019e5a15c138f82b2d1d0b3cb0c
Patrick <patrick.aigner@tum.de>
2024-03-13 08:37:18 +0100

    refactoring, add daily mean calc

--- notebooks/utils/__init__.py
+++ notebooks/utils/__init__.py
@@ -3,3 +3,4 @@ from . import ambient_parameter_conversion
 from . import plot_data
 from . import hermes_download_client
 from . import calibration_processing
+from . import sbs_times
--- notebooks/utils/sbs_times.py
+++ notebooks/utils/sbs_times.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-ongoing_sbs = datetime(2024, 3, 11, 23, 59, 59).replace(tzinfo=timezone.utc)
+ongoing_side_by_side = datetime(2024, 3, 11, 23, 59, 59).replace(tzinfo=timezone.utc)
 
 sbs_times = [
     (
@@ -14,22 +14,46 @@ sbs_times = [
         datetime(2024, 1, 13, 0, 0, 0).replace(tzinfo=timezone.utc),
         datetime(2024, 2, 18, 23, 59, 59).replace(tzinfo=timezone.utc),
     ),
-    (4, datetime(2024, 2, 14, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
+    (
+        4,
+        datetime(2024, 2, 14, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
     (
         5,
         datetime(2024, 2, 7, 0, 0, 0).replace(tzinfo=timezone.utc),
         datetime(2024, 2, 25, 23, 59, 59).replace(tzinfo=timezone.utc),
     ),
-    (6, datetime(2024, 2, 20, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
-    (7, datetime(2024, 2, 21, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
+    (
+        6,
+        datetime(2024, 2, 20, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
+    (
+        7,
+        datetime(2024, 2, 21, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
     (
         8,
         datetime(2024, 2, 13, 0, 0, 0).replace(tzinfo=timezone.utc),
         datetime(2024, 3, 11, 23, 59, 59).replace(tzinfo=timezone.utc),
     ),
-    (9, datetime(2024, 2, 12, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
-    (10, datetime(2024, 1, 13, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
-    (11, datetime(2024, 1, 12, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
+    (
+        9,
+        datetime(2024, 2, 12, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
+    (
+        10,
+        datetime(2024, 1, 13, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
+    (
+        11,
+        datetime(2024, 1, 12, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
     (
         12,
         datetime(2023, 12, 23, 0, 0, 0).replace(tzinfo=timezone.utc),
@@ -40,8 +64,16 @@ sbs_times = [
         datetime(2024, 1, 13, 0, 0, 0).replace(tzinfo=timezone.utc),
         datetime(2024, 1, 30, 23, 59, 59).replace(tzinfo=timezone.utc),
     ),
-    (14, datetime(2024, 3, 2, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
-    (15, datetime(2024, 2, 21, 0, 0, 0).replace(tzinfo=timezone.utc), ongoing_sbs),
+    (
+        14,
+        datetime(2024, 3, 2, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
+    (
+        15,
+        datetime(2024, 2, 21, 0, 0, 0).replace(tzinfo=timezone.utc),
+        ongoing_side_by_side,
+    ),
     (
         16,
         datetime(2023, 12, 23, 0, 0, 0).replace(tzinfo=timezone.utc),
