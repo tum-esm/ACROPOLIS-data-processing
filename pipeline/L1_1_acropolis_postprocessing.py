4fbb6769108cbb58a76e1c381c402a72ada721a2
Patrick Aigner <patrick.aigner@tum.de>
2026-06-03 10:47:15 +0200

    add run() function and main file

---
+++ pipeline/L1_1_acropolis_postprocessing.py
@@ -0,0 +1,138 @@
+import polars as pl
+import polars.selectors as cs
+import gc
+import time
+import logging
+import os
+from datetime import datetime
+
+from utils.config_files import load_json_config
+from utils.import_data import import_acropolis_system_data
+from utils.filter_system_data import extract_wind_data, extraxt_auxilliary_data, extract_edge_calibration_data, extract_measurement_data, extract_calibration_data
+from utils.dilution_correction import wet_to_dry_mole_fraction
+from utils.os_functions import ensure_data_dir
+from utils.calibration_processing import calculate_slope_intercept, apply_slope_intercept, calculate_offset_low_high, apply_offset_low_high
+from utils.dataframe_operations import join_slice
+from utils.write_parquet import write_split_years
+
+from utils.paths import POSTPROCESSED_DATA_DIRECTORY, THINGSBOARD_DATA_DIRECTORY, LOG_DIRECTORY
+
+assert (os.path.exists(THINGSBOARD_DATA_DIRECTORY))
+ensure_data_dir(POSTPROCESSED_DATA_DIRECTORY)
+ensure_data_dir(LOG_DIRECTORY)
+
+config = load_json_config("config.json")
+
+# Create a log file with the current date (YYYY-MM-DD)
+log_filename = os.path.join(LOG_DIRECTORY,
+                            f"{datetime.now().strftime('%Y-%m-%d')}.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=
+    "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler(
+        )  # This allows logs to be printed to the console as well
+    ])
+
+def run():
+    logging.info("=========================================")
+    logging.info("Starting processing of ACROPOLIS raw data")
+
+    # Record start time
+    start_time = time.time()
+    start_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    logging.info(f"Script started at: {start_datetime}")
+
+    for id in config["postprocessing"]["system_ids"]:
+        logging.info(f"Processing system with id: {id}")
+        # Import system data
+        df_raw = import_acropolis_system_data(
+            years=config["postprocessing"]["input_years"],
+            target_directory=THINGSBOARD_DATA_DIRECTORY,
+            id=id,
+        )
+
+        # Extract data
+        df_lazy = extract_measurement_data(df_raw)
+        df_wind = extract_wind_data(df_raw)
+        df_aux = extraxt_auxilliary_data(df_raw)
+        df_edge_cal = extract_edge_calibration_data(df_raw)
+        df_calibration = extract_calibration_data(df_raw)
+
+        # Save cal data
+        write_split_years(df=df_calibration,
+                        id=id,
+                        target_directory=POSTPROCESSED_DATA_DIRECTORY,
+                        prefix="Cal_1min")
+
+        # Calculate slope and intercept
+        df_slope_intercept = calculate_slope_intercept(df_calibration)
+        
+        # Evaluate if one-point correction should be applied
+        if config["postprocessing"]["add_1P_correction"]:
+            df_offset_low_high = calculate_offset_low_high(df_calibration)
+        else:
+            df_offset_low_high = pl.DataFrame()
+        run_one_point = config["postprocessing"]["add_1P_correction"]
+
+
+        del df_calibration
+        gc.collect()  # Explicitly run garbage collection
+
+        # Aggregate to 1 minute intervals
+        df = df_lazy.group_by_dynamic("datetime", every='1m', group_by=["system_id", "system_name"]) \
+                .agg(cs.numeric().mean()) \
+                .collect()
+
+        # Process measurement data
+        df = df.pipe(wet_to_dry_mole_fraction) \
+            .pipe(apply_slope_intercept, df_slope_intercept) \
+            .pipe(apply_offset_low_high, df_offset_low_high, run_one_point) \
+            .pipe(join_slice, df_wind, "2m") \
+            .pipe(join_slice, df_aux, "2m") \
+            .pipe(join_slice, df_edge_cal, "1d") \
+            .drop("^.*_right$") \
+            .sort("datetime")
+
+        # Save data
+        logging.info(f"Writing 1m data to parquet. Length: {len(df)}")
+        write_split_years(df=df,
+                        id=id,
+                        target_directory=POSTPROCESSED_DATA_DIRECTORY,
+                        prefix="1min")
+
+        # Aggregate to 1 hour intervals
+        df_1h = df.sort("datetime") \
+                .group_by_dynamic("datetime", every='1h', group_by=["system_id", "system_name"]) \
+                .agg([
+                    cs.numeric().mean(),
+                    pl.col("gmp343_corrected").std().alias("gmp343_corrected_std"),
+                    pl.col("gmp343_corrected").var().alias("gmp343_corrected_var")
+                ])
+
+        # Save data
+        logging.info(f"Writing 1h data to parquet. Length: {len(df_1h)}")
+        write_split_years(df=df_1h,
+                        id=id,
+                        target_directory=POSTPROCESSED_DATA_DIRECTORY,
+                        prefix="1h")
+
+        # Clear memory
+        del df, df_1h
+        gc.collect()  # Explicitly run garbage collection
+
+    # Record end time
+    end_time = time.time()
+    duration = end_time - start_time
+    end_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    logging.info(f"Script ended at: {end_datetime}")
+    logging.info(f"Total duration: {duration:.2f} seconds")
+
+
+if __name__ == "__main__":
+    run()
---
+++ pipeline/L1_3_write_csv_icos_cp.py
@@ -0,0 +1,77 @@
+import polars as pl
+import os
+import gc
+import time
+import logging
+from datetime import datetime
+
+from utils.config_files import load_json_config
+from utils.import_data import import_acropolis_site_data
+from utils.dataframe_operations import convert_to_1min_icos_cp_format
+from utils.icos_cp_csv_conversion import df_to_L1_1min_icos_csv
+from config.sites_deloyment_times import deployment_times
+
+from utils.paths import DESPIKED_DATA_DIRECTORY, CONFIG_DIRECTORY, LOG_DIRECTORY
+
+assert (os.path.exists(DESPIKED_DATA_DIRECTORY))
+
+# load files
+config = load_json_config("config.json")
+
+path = os.path.join(CONFIG_DIRECTORY, "sites.csv")
+sites_meta = pl.read_csv(path, separator=";").with_columns(
+    pl.exclude(pl.Utf8).cast(str))
+
+# Create a log file with the current date (YYYY-MM-DD)
+log_filename = os.path.join(LOG_DIRECTORY,
+                            f"{datetime.now().strftime('%Y-%m-%d')}.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=
+    "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler(
+        )  # This allows logs to be printed to the console as well
+    ])
+
+def run():
+
+    logging.info("=========================================")
+    logging.info("Starting extraction of site data and *.csv conversion.")
+
+    # Record start time
+    start_time = time.time()
+    start_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    logging.info(f"Script started at: {start_datetime}")
+
+    for site in config["icos_cities_portal"]["site_names"]:
+        
+        logging.info(f"Processing site: {site}")
+        df = import_acropolis_site_data(target_directory=DESPIKED_DATA_DIRECTORY,
+                                        deployment_times=deployment_times,
+                                        site_name=site)
+
+        # Convert DF to ICOS CP format
+        df = df.pipe(convert_to_1min_icos_cp_format)
+
+        # Write to CSV with ICOS CP Header
+        df_to_L1_1min_icos_csv(df=df, sites_meta=sites_meta, site=site)
+
+        # Clear memory
+        del df
+        gc.collect()  # Explicitly run garbage collection
+
+    # Record end time
+    end_time = time.time()
+    duration = end_time - start_time
+    end_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    logging.info(f"Script ended at: {end_datetime}")
+    logging.info(f"Total duration: {duration:.2f} seconds")
+
+
+if __name__ == "__main__":
+    run()
